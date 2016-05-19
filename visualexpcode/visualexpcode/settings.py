4d9fe6e5cc4467f33971e0b78157436552a45e39
Gael Le Bon <gael.m.lebon@gmail.com>
2016-05-19 13:39:59 +0200

    Added visualUser table

--- visualexpcode/visualexpcode/settings.py
+++ visualexpcode/visualexpcode/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'visualAdmin.apps.VisualadminConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
