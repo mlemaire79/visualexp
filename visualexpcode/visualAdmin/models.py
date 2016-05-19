71317d848ab27e0a1b35585d9a13042071ccccb3
Mathieu <lemaire.mathieu79@gmail.com>
2016-05-19 14:08:16 +0200

    Model Artist

--- visualexpcode/visualAdmin/models.py
+++ visualexpcode/visualAdmin/models.py
@@ -11,3 +11,13 @@ class visualUser(models.Model):
 	def __str__(self):
 		return self.user.first_name + self.user.last_name
 
+class artist(models.Model):
+	artist_id = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=64)
+	last_name = models.CharField(max_length=64)
+	stage_name = models.CharField(max_length=128)
+	birth_date = models.DateField()
+
+	def __str__(self):
+		return self.stage_name
+
