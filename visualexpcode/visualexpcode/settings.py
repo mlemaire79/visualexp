72654916eb25bcd598e63bb0846bbe2873a515c5
Gael Le Bon <gael.m.lebon@gmail.com>
2016-06-04 13:55:46 +0200

    Added directory structure and settings for user uploads

--- visualexpcode/visualexpcode/settings.py
+++ visualexpcode/visualexpcode/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/visualexp/static'
+
+# User Uploaded Content ( For artworks )
+# https://docs.djangoproject.com/en/1.9/ref/settings/#std:setting-MEDIA_ROOT
+# https://docs.djangoproject.com/en/1.9/ref/models/fields/#filefield
+MEDIA_ROOT = '/media/'
