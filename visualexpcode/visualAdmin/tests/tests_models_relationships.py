1ab0e8ffb259d76540f7f9e05b954e0285cad91f
Gael Le Bon <gael.m.lebon@gmail.com>
2016-06-16 17:05:25 +0200

    Added tests for artwork, and implemented for Artwork relationships

--- visualexpcode/visualAdmin/tests/tests_models_relationships.py
+++ visualexpcode/visualAdmin/tests/tests_models_relationships.py
@@ -12,13 +12,19 @@ class ManyToManyArtistArtworks(TestCase):
         aw1 =VideoArtwork.objects.create(title="TitleAW1", description="DescriptionAW1", length=100)
         #Artwork2 has 2 artists
         aw2 = ImageArtwork.objects.create(title="TitleAW2", description="DescAW2")
-        aw1.artists.add(ar1)
-        aw2.artists.add(ar1)
-        aw2.artists.add(ar2)
+        ar1.artworks.add(aw1)
+        ar2.artworks.add(aw1)
+        ar1.artworks.add(aw2)
         
 
     def test_getArtworkListFromArtist(self):
-        self.fail("Not implemented")
+        artist1 = Artist.objects.get(first_name="TestFN1")
+        artworkList = artist1.artworks.all()
+        self.assertEqual(len(artworkList), 2)
+
+        artist2 = Artist.objects.get(first_name="TestFN2")
+        artworkList = artist2.artworks.all()
+        self.assertEqual(len(artworkList), 1, "Artist 2 only has 1 artwork")
 
 
     def test_get_artwork_details_from_list(self):
