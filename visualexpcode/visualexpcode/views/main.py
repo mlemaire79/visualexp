990a993bdf73dd0c6adef1cdb0b1df193e4d08a4
Cédric LEVEQUE <cedric.leveque.pro@gmail.com>
2016-07-25 15:01:32 +0200

    Templates avec objets

--- visualexpcode/visualexpcode/views/main.py
+++ visualexpcode/visualexpcode/views/main.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django.views.generic import View
+from django.views.generic import ListView
+from visualAdmin.models import Exposition
+from django.views.decorators.cache import cache_page
 
 class Homepage(View):
 
@@ -12,11 +15,19 @@ class Homepage(View):
 class Current(View):
 
     def get(self, request):
+        current_expo = Exposition.get_current()
+        context = {
+            'current_expo': current_expo,
+        }
         template = loader.get_template('extends/currexposition.html')
-        return HttpResponse(template.render(request))
+        return HttpResponse(template.render(context, request))
 
 class Listing(View):
 
     def get(self, request):
+        current_expo = Exposition.get_current()
+        context = {
+            'current_expo': current_expo,
+        }
         template = loader.get_template('extends/listartworks.html')
-        return HttpResponse(template.render(request))
+        return HttpResponse(template.render(context, request))
